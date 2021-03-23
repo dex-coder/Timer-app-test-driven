8711e28623c0b2ab4e2a924156a69e9218dfecbe
dex-coder <mhdkashifkhan028@gmail.com>
2021-03-23 18:09:15 +0500

    all components

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Timer from '../Timer/Timer';
+
+function App() {
+  return (
+    <div className="App">
+     <Timer />
+    </div>
+  );
+}
+
+export default App;
